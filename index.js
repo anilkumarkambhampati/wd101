b5aa29ee0348a0520f460a0e0eb943caf8edda4e
anilkumarkambhampati <151641661+anilkumarkambhampati@users.noreply.github.com>
2023-11-22 18:13:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,65 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('user_form');
-    const tableBody = document.querySelector('#userTable tbody');
-  
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      const namecell = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      const dobInput = document.getElementById('dob');
-      const dob = new Date(dobInput.value);
-      const terms = document.getElementById('acceptterms').checked;
-
-      const today = new Date();
-      const age = today.getFullYear() - dob.getFullYear();
-      const monthDiff = today.getMonth() - dob.getMonth();
-      if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
-        age--;
-      }
-  
-      if (age < 18 || age > 55) {
-        alert('Age must be between 18 and 55.');
-        return;
-      }
-      const newRow = tableBody.insertRow();
-      newRow.innerHTML = `
-        <td>${namecell}</td>
-        <td>${email}</td>
-        <td>${password}</td>
-        <td>${dob.toISOString().slice(0, 10)}</td>
-        <td>${terms ? 'true' : 'false'}</td>
-      `;
-      const userData = { namecell, email, password, dob: dob.toISOString().slice(0, 10), terms };
-      localStorage.setItem('userData', JSON.stringify(userData));
-      form.reset();
-    });
-    const savedD = JSON.parse(localStorage.getItem('userData'));
-    if (savedD) {
-      const newRow = tableBody.insertRow();
-      newRow.innerHTML = `
-        <td>${savedD.namecell}</td>
-        <td>${savedD.email}</td>
-        <td>${savedD.password}</td>
-        <td>${savedD.dob}</td>
-        <td>${savedD.terms ? 'true' : 'false'}</td>
-      `;
+let userForm=document.getElementById("user_form");
+const retrieve=() =>{
+    let entries=localStorage.getItem("user_entries");
+    if(entries){
+        entries=JSON.parse(entries);
+    }else{
+        entries=[];
     }
-  });
+    return entries;
+}
+let userEntries=retrieve();
+const displayEntries = () => {
+    const entries = retrieve();
+    const tableEntries = entries.map((entry) => {
+        const nameCell = `<td>${entry.name}</td>`;
+        const emailCell = `<td>${entry.email}</td>`;
+        const passwordCell = `<td>${entry.password}</td>`;
+        const dobCell = `<td>${entry.dob}</td>`;
+        const acceptTermsCell = `<td>${entry.acceptedTermsAndconditions ? true : false}</td>`;
+
+        const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+        userForm.reset();
+        return row;
+    }).join("\n");
+
+    const table =` <table width="60%"><tr><th>Name</th><th>Email</th><th>Password</th><th>Dob</th><th>Accepted terms? </th></tr>${tableEntries}</table>`;
+    let details = document.getElementById("user_entries");
+    details.innerHTML = table;
+}
+const calculateAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+};
+const saveUserForm=(event) =>{
+    event.preventDefault();
+    const name=document.getElementById("name").value;
+    const email=document.getElementById("email").value;
+    const password=document.getElementById("password").value;
+    const dob=document.getElementById("dob").value;
+    const acceptedTermsAndconditions=document.getElementById("acceptterms").checked;
+    const age = calculateAge(dob);
+    if (age < 18 || age > 55) {
+        alert("Age must be between 18 and 55 years.");
+        return;
+    }
+    const entry={
+        name,
+        email,
+        password,
+        dob,
+        acceptedTermsAndconditions
+    };
+    userEntries.push(entry);
+    localStorage.setItem("user_entries",JSON.stringify(userEntries));
+    displayEntries();
+}
+userForm.addEventListener("submit",saveUserForm);
+displayEntries();
